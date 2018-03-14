b32115267f853461c6a0bed245020343fa1ed775
Vestimir Markov <me@vestimir.com>
2018-03-14 09:08:18 +0200

    Improve image

--- src/lib/Image.js
+++ src/lib/Image.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Img = ({ src, variant, ...rest }) => {
-  if (typeof src === 'object' && (src.name.includes('.svg') || src.name.includes('.gif'))) return (<img src={src.urls.original} alt={src.alt} {...rest} />);
-  if (typeof src === 'string') return (<img src={src} alt={src} {...rest} />);
-  if (typeof src === 'object') return (<img src={src.urls[variant]} alt={src.alt} {...rest} />);
+const Img = ({ src, variant, className, onLoad }) => {
+  if (typeof src === 'object' && (src.name.includes('.svg') || src.name.includes('.gif'))) return (<img src={src.urls.original} alt={src.alt} className={className} onLoad={onLoad} />);
+  if (typeof src === 'string') return (<img src={src} alt={src} className={className} onLoad={onLoad} />);
+  if (typeof src === 'object') return (<img src={src.urls[variant]} alt={src.alt} className={className} onLoad={onLoad} />);
 
   return null;
 };
