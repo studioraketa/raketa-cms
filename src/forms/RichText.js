cfbb7ea0a685ead43ce317d8597c9e2dfd968716
Vestimir Markov <me@vestimir.com>
2018-03-19 17:22:22 +0200

    Update package

--- src/forms/RichText.js
+++ src/forms/RichText.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
+
+
+import { Editor, EditorState, convertToRaw, ContentState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
-
-const TOOLBAR_CONFIG = {
-  options: ['blockType', 'inline', 'list', 'link', 'remove', 'history'],
-};
 
 class RichText extends React.Component {
   constructor(props) {
@@ -34,19 +31,67 @@ class RichText extends React.Component {
   };
 
   render() {
-    const { editorState } = this.state;
-
     return (
       <Editor
-        toolbar={TOOLBAR_CONFIG}
-        editorState={editorState}
-        onEditorStateChange={editorState => this.onEditorStateChange(editorState)}
+        editorState={this.state.editorState}
+        onChange={state => this.onEditorStateChange(state)}
       />
-    )
+    );
   }
 }
 
-// class RichText2 extends React.Component {
+// import RichTextEditor from 'react-rte';
+
+// var RichTextEditor;
+// if (typeof(window) !== 'undefined') { RichTextEditor = require('react-rte').default; }
+
+// import { EditorState, convertToRaw, ContentState } from 'draft-js';
+// import { Editor } from 'react-draft-wysiwyg';
+// import draftToHtml from 'draftjs-to-html';
+// import htmlToDraft from 'html-to-draftjs';
+
+// const TOOLBAR_CONFIG = {
+//   options: ['blockType', 'inline', 'list', 'link', 'remove', 'history'],
+// };
+
+// class RichText extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     const contentBlock = htmlToDraft(props.value);
+
+//     if (contentBlock) {
+//       const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+//       const editorState = EditorState.createWithContent(contentState);
+//       this.state = {
+//         editorState,
+//       };
+//     }
+//   }
+
+//   onEditorStateChange(newState) {
+//     const { onChange } = this.props;
+//     const { editorState } = this.state;
+
+//     this.setState({ editorState: newState }, () => {
+//       if (onChange) onChange(draftToHtml(convertToRaw(newState.getCurrentContent())));
+//     });
+//   };
+
+//   render() {
+//     const { editorState } = this.state;
+
+//     return (
+//       <Editor
+//         toolbar={TOOLBAR_CONFIG}
+//         editorState={editorState}
+//         onEditorStateChange={editorState => this.onEditorStateChange(editorState)}
+//       />
+//     )
+//   }
+// }
+
+// class RichText extends React.Component {
 //   constructor(props) {
 //     super(props);
 
@@ -75,16 +120,16 @@ class RichText extends React.Component {
 //   }
 // }
 
-// RichText.defaultProps = {
-//   value: '',
-//   placeholder: '',
-// };
+RichText.defaultProps = {
+  value: '',
+  placeholder: '',
+};
 
-// RichText.propTypes = {
-//   label: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired,
-//   value: PropTypes.string,
-//   placeholder: PropTypes.string,
-// };
+RichText.propTypes = {
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+};
 
 export default RichText;
