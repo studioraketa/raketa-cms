b7f02f089cd398758b6cd9d4e3e0c57978097ec1
Vestimir Markov <me@vestimir.com>
2019-06-18 15:48:11 +0300

    Update bundle

--- src/helpers/imageSrc.js
+++ src/helpers/imageSrc.js
@@ -1,5 +1,5 @@
 const imgageSrc = (src, variant) => {
-  if (typeof src === 'object' && !('name' in src)) console.warn(`WARNING: Image with empty {}: ${variant} ${className || ''}`);
+  if (typeof src === 'object' && !('name' in src)) console.warn(`WARNING: Image with empty {}: ${variant}`);
 
   if (typeof src === 'object' && ('name' in src) && (src.name.includes('.svg') || src.name.includes('.gif'))) return src.urls.original;
   if (typeof src === 'object' && ('name' in src)) return src.urls[variant];
