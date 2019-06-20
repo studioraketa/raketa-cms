b7773aa10c2eb60d91b209cab6cc94d8dc86450c
Vestimir Markov <me@vestimir.com>
2019-06-20 13:31:14 +0300

    Add HTML option for rich editor

--- src/forms/RichText.js
+++ src/forms/RichText.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { FormControl, Label } from 'raketa-ui';
+import {
+  FormControl,
+  Label,
+  Button,
+} from 'raketa-ui';
+import TextArea from './TextArea';
 
 class RichText extends React.Component {
   constructor(props) {
@@ -7,6 +12,7 @@ class RichText extends React.Component {
 
     this.state = {
       editorValue: null,
+      mode: 'rich',
     }
 
     this.RichTextEditor = null;
@@ -16,34 +22,68 @@ class RichText extends React.Component {
     this.RichTextEditor = require('@dock365/react-rte').default;
 
     this.setState({
+      plainValue: this.props.value,
       editorValue: this.RichTextEditor.createValueFromString(this.props.value, 'html'),
     });
   }
 
   onChange(editorValue) {
     const { onChange } = this.props;
+
     this.setState({ editorValue }, () => {
       if (onChange) onChange(editorValue.toString('html'));
     });
   }
 
+  updatePlainValue(plainValue) {
+    const { onChange } = this.props;
+
+    this.setState({
+      plainValue,
+      editorValue: this.RichTextEditor.createValueFromString(plainValue, 'html'),
+    }, () => {
+      if (onChange) onChange(plainValue);
+    });
+  }
+
   render() {
-    const { label, placeholder } = this.props;
-    const { editorValue } = this.state;
+    const { label, placeholder, onChange } = this.props;
+    const { editorValue, plainValue, mode } = this.state;
     const RichTextEditor = this.RichTextEditor;
 
-    return (
-      <FormControl>
-        {label &&
-          <Label>{label}</Label>}
+    const activeStyle = { marginRight: 0, backgroundColor: '#000', color: '#fff' };
+    const inactiveStyle = { marginRight: 0, backgroundColor: '#fff', color: '#000' };
 
-          {RichTextEditor &&
-            <RichTextEditor
-              value={editorValue}
-              onChange={(v) => this.onChange(v)}
-            />
-          }
-      </FormControl>
+    return (
+      <React.Fragment>
+        {(mode === 'rich') && (
+          <FormControl>
+            {label &&
+              <Label>{label}</Label>}
+
+            {RichTextEditor &&
+              <RichTextEditor
+                value={editorValue}
+                onChange={(v) => this.onChange(v)}
+              />
+            }
+          </FormControl>
+        )}
+
+        {(mode === 'html') && (
+          <TextArea
+            label={label}
+            placeholder={placeholder}
+            value={plainValue}
+            onChange={(v) => this.updatePlainValue(v)}
+          />
+        )}
+
+        <div style={{ marginBottom: '14px', marginTop: '-14px' }}>
+          <Button type="button" sm onClick={() => this.setState({ mode: 'rich' })} style={(mode === 'rich') ? activeStyle : inactiveStyle}>Rich</Button>
+          <Button type="button" sm onClick={() => this.setState({ mode: 'html' })} style={(mode === 'html') ? activeStyle : inactiveStyle}>HTML</Button>
+        </div>
+      </React.Fragment >
     );
   }
 }
