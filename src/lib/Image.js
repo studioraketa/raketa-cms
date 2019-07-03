0ec65c8857e7a39df61b9197759b44702b6015e5
Nedko Slavov <n.p.slavov@gmail.com>
2019-07-03 12:53:47 +0300

    Alt add src.name as last default

--- src/lib/Image.js
+++ src/lib/Image.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Img = ({ src, variant, className, onLoad, alt = null }) => {
+  const imgAlt = alt !== null ? alt : src.alt;
+
   if (typeof src === 'object' && !('name' in src)) console.warn(`WARNING: Image with empty {}: ${variant} ${className || ''}`);
 
-  if (typeof src === 'object' && ('name' in src) && (src.name.includes('.svg') || src.name.includes('.gif'))) return (<img src={src.urls.original} alt={alt !== null ? alt : src.alt} className={className} onLoad={onLoad} />);
-  if (typeof src === 'object' && ('name' in src)) return (<img src={src.urls[variant]} alt={alt !== null ? alt : src.alt} className={className} onLoad={onLoad} />);
-  if (typeof src === 'string') return (<img src={src} alt={src} className={className} onLoad={onLoad} />);
+  if (typeof src === 'object' && ('name' in src) && (src.name.includes('.svg') || src.name.includes('.gif'))) return (<img src={src.urls.original} alt={imgAlt !== null ? imgAlt : src.name} className={className} onLoad={onLoad} />);
+  if (typeof src === 'object' && ('name' in src)) return (<img src={src.urls[variant]} alt={imgAlt !== null ? imgAlt : src.name} className={className} onLoad={onLoad} />);
+  if (typeof src === 'string') return (<img src={src} alt={imgAlt !== null ? imgAlt : src.name} className={className} onLoad={onLoad} />);
 
   return null;
 };
