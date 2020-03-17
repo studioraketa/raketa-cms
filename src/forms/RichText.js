04db02728566f71317038f80ee1a47fb969ced93
Vestimir Markov <me@vestimir.com>
2020-03-17 22:28:14 +0200

    Remove react-rte

--- src/forms/RichText.js
+++ src/forms/RichText.js
@@ -1,97 +1,13 @@
 import React from 'react';
-import {
-  FormControl,
-  Label,
-  Button,
-} from 'raketa-ui';
-import TextArea from './TextArea';
 
-class RichText extends React.Component {
-  constructor(props) {
-    super(props);
+export default () => (
+  <div>
+    <p>RichText is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-rte</p>
 
-    this.state = {
-      editorValue: null,
-      mode: 'rich',
-    }
+    <strong>Update your imports</strong>
+    <code>{`import {RichText} from '@raketa-cms/raketa-rte';`}</code>
 
-    this.RichTextEditor = null;
-  }
-
-  componentDidMount() {
-    this.RichTextEditor = require('@dock365/react-rte').default;
-
-    this.setState({
-      plainValue: this.props.value,
-      editorValue: this.RichTextEditor.createValueFromString(this.props.value, 'html'),
-    });
-  }
-
-  onChange(editorValue) {
-    const { onChange } = this.props;
-
-    this.setState({ editorValue }, () => {
-      if (onChange) onChange(editorValue.toString('html'));
-    });
-  }
-
-  updatePlainValue(plainValue) {
-    const { onChange } = this.props;
-
-    this.setState({
-      plainValue,
-      editorValue: this.RichTextEditor.createValueFromString(plainValue, 'html'),
-    }, () => {
-      if (onChange) onChange(plainValue);
-    });
-  }
-
-  render() {
-    const { label, placeholder, onChange } = this.props;
-    const { editorValue, plainValue, mode } = this.state;
-    const RichTextEditor = this.RichTextEditor;
-
-    const activeStyle = { marginRight: 0, backgroundColor: '#000', color: '#fff' };
-    const inactiveStyle = { marginRight: 0, backgroundColor: '#fff', color: '#000' };
-
-    return (
-      <React.Fragment>
-        {(mode === 'rich') && (
-          <FormControl>
-            {label &&
-              <Label>{label}</Label>}
-
-            {RichTextEditor &&
-              <RichTextEditor
-                value={editorValue}
-                onChange={(v) => this.onChange(v)}
-              />
-            }
-          </FormControl>
-        )}
-
-        {(mode === 'html') && (
-          <TextArea
-            label={label}
-            placeholder={placeholder}
-            value={plainValue}
-            onChange={(v) => this.updatePlainValue(v)}
-          />
-        )}
-
-        <div style={{ marginBottom: '14px', marginTop: '-14px' }}>
-          <Button type="button" sm onClick={() => this.setState({ mode: 'rich' })} style={(mode === 'rich') ? activeStyle : inactiveStyle}>Rich</Button>
-          <Button type="button" sm onClick={() => this.setState({ mode: 'html' })} style={(mode === 'html') ? activeStyle : inactiveStyle}>HTML</Button>
-        </div>
-      </React.Fragment >
-    );
-  }
-}
-
-RichText.defaultProps = {
-  label: 'Text',
-  value: '',
-  placeholder: '',
-};
-
-export default RichText;
+    <strong>Update usage in JSON adminFields</strong>
+    <code>{`text: { type: 'custom', component: RichText },`}</code>
+  </div>
+);
