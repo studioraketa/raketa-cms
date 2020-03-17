d68ebb30bdf2a5ce5d6aea84a0fc8dd233f48d27
Vestimir Markov <me@vestimir.com>
2020-03-17 23:03:14 +0200

    Remove image picker from core

--- src/MediaManager.js
+++ src/MediaManager.js
@@ -1,39 +1,5 @@
-const serialize = (params) => Object.keys(params)
-  .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-  .join('&');
-
-const get = (url, params = {}, opts = {}) => fetch(`${url}?${serialize(params)}`, opts);
-const patch = (url, params = {}, opts = {}) => fetch(url, Object.assign({ method: 'PATCH', credentials: 'same-origin', body: JSON.stringify(params) }, opts))
-const post = (url, params = {}, opts = {}) => fetch(url, Object.assign({ method: 'POST', credentials: 'same-origin', body: JSON.stringify(params) }, opts))
-const del = (url, params = {}, opts = {}) => fetch(url, Object.assign({ method: 'DELETE', credentials: 'same-origin', body: JSON.stringify(params) }, opts))
-
 export default class MediaManager {
-  constructor(baseUrl) {
-    this.baseUrl = baseUrl;
-  }
-
-  findAll(callback, params = {}) {
-    get(`${this.baseUrl}images`, params).then(r => r.json()).then(images => callback(images));
-  }
-
-  uploadFile(file, callback) {
-    const body = new FormData();
-    body.append('image[asset]', file);
-
-    post(`${this.baseUrl}images`, {}, { body })
-      .then(r => r.json())
-      .then(image => callback(image));
-  }
-
-  update(image, params, callback) {
-    patch(`${this.baseUrl}images/${image.id}`, { image: params }, { headers: { 'content-type': 'application/json' } })
-      .then(r => r.json())
-      .then(image => callback(image));
-  }
-
-  destroy(image, callback) {
-    del(`${this.baseUrl}images/${image.id}`, {}, { headers: { 'content-type': 'application/json' } })
-      .then(r => r.json())
-      .then(image => callback(image));
+  constructor() {
+    console.warn('MediaManager is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-image-input. ');
   }
 }
--- src/pickers/ImagePicker/ImageInput.js
+++ src/pickers/ImagePicker/ImageInput.js
@@ -1,41 +1,7 @@
 import React from 'react';
-import { RaketaUIProvider } from 'raketa-ui';
 
-import ImagePicker from './ImagePicker';
-
-class ImageInput extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectedImage: props.image || false,
-    };
-  }
-
-  handleChange(selectedImage) {
-    this.setState({ selectedImage }, () => {
-      if (this.props.onChange) this.props.onChange(selectedImage);
-    })
-  }
-
-  render() {
-    const { name, mediaManager } = this.props;
-    const { selectedImage } = this.state;
-
-    return (
-      <RaketaUIProvider>
-        <div>
-          <ImagePicker
-            mediaManager={mediaManager}
-            value={selectedImage}
-            onChange={selectedImage => this.handleChange(selectedImage)}
-          />
-
-          <input type="hidden" name={name} value={selectedImage ? selectedImage.id : ''} />
-        </div>
-      </RaketaUIProvider>
-    );
-  }
-}
-
-export default ImageInput;
+export default () => (
+  <div>
+    <p>ImageInput is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-image-input. </p>
+  </div>
+);
