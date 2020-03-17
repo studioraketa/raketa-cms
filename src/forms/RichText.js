44672ef767699bb365687ec27aac68de1345d1cf
Vestimir Markov <me@vestimir.com>
2020-03-17 22:31:50 +0200

    Improve transitional message for react-rte

--- src/forms/RichText.js
+++ src/forms/RichText.js
@@ -5,9 +5,9 @@ export default () => (
     <p>RichText is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-rte</p>
 
     <strong>Update your imports</strong>
-    <code>{`import {RichText} from '@raketa-cms/raketa-rte';`}</code>
+    <p><code>{`import {RichText} from '@raketa-cms/raketa-rte';`}</code></p>
 
     <strong>Update usage in JSON adminFields</strong>
-    <code>{`text: { type: 'custom', component: RichText },`}</code>
+    <p><code>{`text: { type: 'custom', component: RichText },`}</code></p>
   </div>
 );
