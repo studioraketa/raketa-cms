0ea360bdf742e242b170e135ce73ab05f8a3ccee
Vestimir Markov <me@vestimir.com>
2020-03-17 23:08:40 +0200

    Update image picker instructions

--- src/pickers/ImagePicker/ImageInput.js
+++ src/pickers/ImagePicker/ImageInput.js
@@ -3,5 +3,8 @@ import React from 'react';
 export default () => (
   <div>
     <p>ImageInput is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-image-input. </p>
+
+    <strong>Update imports</strong>
+    <p><code>{`import { ImageInput } from '@raketa-cms/raketa-image-picker';`}</code></p>
   </div>
 );
--- src/pickers/ImagePicker/ImagePicker.js
+++ src/pickers/ImagePicker/ImagePicker.js
@@ -3,5 +3,11 @@ import React from 'react';
 export default () => (
   <div>
     <p>ImagePicker is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-image-input. </p>
+
+    <strong>Update imports</strong>
+    <p><code>{`import { ImagePicker } from '@raketa-cms/raketa-image-picker';`}</code></p>
+
+    <strong>Update JSON adminFields</strong>
+    <p><code>{`image: { type: 'image', component: ImagePicker },`}</code></p>
   </div>
 );
