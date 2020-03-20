e3dfb10c8e8d3d0e0ff79b68155603644c67eb15
Vestimir Markov <me@vestimir.com>
2020-03-20 08:32:22 +0200

    Update media manager deprecations

--- src/MediaManager.js
+++ src/MediaManager.js
@@ -1,5 +1,13 @@
+console.warn('MediaManager is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-image-input. ');
+
 export default class MediaManager {
-  constructor() {
-    console.warn('MediaManager is no longer part of @raketa-cms/raketa-cms. Please use @raketa-cms/raketa-image-input. ');
-  }
+  constructor() { }
+
+  findAll(callback, params = {}) { }
+
+  uploadFile(file, callback) { }
+
+  update(image, params, callback) { }
+
+  destroy(image, callback) { }
 }
