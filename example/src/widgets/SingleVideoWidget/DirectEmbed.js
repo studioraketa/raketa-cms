febfa4082d4bcfcd09266276cc5536c5658b0baf
Svetlozar Mihailov <svetliomihailov@gmail.com>
2022-09-14 13:23:16 +0300

    Add an option to display youtube video from their no cookie domain

--- example/src/widgets/SingleVideoWidget/DirectEmbed.js
+++ example/src/widgets/SingleVideoWidget/DirectEmbed.js
@@ -1,39 +1,51 @@
-import React from 'react';
+import React from 'react'
 import { EmbeddedVideo, Img } from '@raketa-cms/raketa-cms'
 
 class DirectEmbed extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      play: false,
-    };
+      play: false
+    }
   }
 
   handlePlay = () => this.setState({ play: true })
 
   render() {
-    const { play } = this.state;
-    const { image, videoUrl } = this.props;
+    const { play } = this.state
+    const { image, videoUrl, noCookieDomains = false } = this.props
 
     return (
-      <div onClick={this.handlePlay} ref={videoWrapper => this.videoWrapper = videoWrapper}>
-        {play || !image ?
-          <div className="full-image full-video" style={{ display: 'block' }}>
-            <EmbeddedVideo videoUrl={videoUrl} autoplay={play} />
+      <div
+        onClick={this.handlePlay}
+        ref={(videoWrapper) => (this.videoWrapper = videoWrapper)}
+      >
+        {play || !image ? (
+          <div className='full-image full-video' style={{ display: 'block' }}>
+            <EmbeddedVideo
+              videoUrl={videoUrl}
+              autoplay={play}
+              noCookieDomains={noCookieDomains}
+            />
           </div>
-          :
-          <div className="full-image-anchor" style={{ cursor: 'pointer' }}>
-            <span className="icon-play-round"></span>
+        ) : (
+          <div className='full-image-anchor' style={{ cursor: 'pointer' }}>
+            <span className='icon-play-round'></span>
 
-            <div className="full-image full-video">
-              <Img loading="lazy" src={image} variant="thumb" className="content" />
+            <div className='full-image full-video'>
+              <Img
+                loading='lazy'
+                src={image}
+                variant='thumb'
+                className='content'
+              />
             </div>
           </div>
-        }
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default DirectEmbed;
+export default DirectEmbed
